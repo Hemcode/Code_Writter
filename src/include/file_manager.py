399cf722cfe4d774d091c021df5d7a120325051a
Hemcode <pro.hemdy@gmail.com>
2023-03-09 17:53:31 +0100

    CodeWritterRestart003

---
+++ src/include/file_manager.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+class File:
+    def __init__(self, file_name: str, file_path: str):
+        self.file_name = file_name
+        self.file_path = file_path
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Entry, Menu, Button, Frame, Label
+from tkinter import *
 
 # Variables and constants
 PROJECT_NAME: str = "Code Writter"
@@ -16,8 +16,16 @@ window.title(PROJECT_NAME)
 window.geometry("640x480")
 window.minsize(480, 360)
 
+# Frames
+file_explorer_frame = Frame(window, bg=very_dark_grey)
+
+# Label
+project_name_dir = Label(file_explorer_frame, bg=black, fg=white, text="Project Dir", font=("Helvetica", 20))
+project_name_dir.pack(side=TOP)
+
 # Configure
 window.config(bg=grey)
 
 # Show objects
+file_explorer_frame.pack(side="left")
 window.mainloop()
